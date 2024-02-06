5fefc15c16eedab025a8e6e96e25d07bf7d8da99
Jurgen Hideni <jurgenhideni@gmail.com>
2024-02-06 13:00:28 +0100

    initial commit

---
+++ src/components/Time/Time.jsx
@@ -0,0 +1,38 @@
+import React,{useState} from "react"
+import '../Time/time.css'
+
+
+const Time = () =>{
+setInterval(updateTime, 1000);
+const now = new Date().toLocaleTimeString();
+const [time,setTime]=useState(now);
+
+const date = new Date().toLocaleDateString();
+
+const options= {weekday: 'long'};
+const daysOfWeek = new Date().toLocaleString('en-US', options)
+
+function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+}
+
+
+
+    return(
+        <div className="container">
+            <div className="clock">
+            <h1>{time}</h1>
+            <h2>{date}</h2>
+            <h2>{daysOfWeek}</h2>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Time
+
+
+
+
